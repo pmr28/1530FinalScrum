8c34b28e95354174267aa49a41fba2f63e052fda
nvp14 <146030859+nvp14@users.noreply.github.com>
2023-12-06 21:58:48 -0500

    added track and record tests

--- test_expenses.py
+++ test_expenses.py
@@ -59,5 +59,75 @@ class FinanceTrackerTestCase(unittest.TestCase):
         self.assertNotIn(b'Logged in as', response.data)
         self.assertIn(b'Sign In Here!', response.data)
 
+    # test for the tracking page
+    def test_income(self):
+        # sign up
+        self.app.post('/signup', data=dict(
+            user='testuser',
+            password='testpassword',
+            password2='testpassword'
+        ), follow_redirects=True)
+
+        # log in
+        self.app.post('/signin', data=dict(
+            user='testuser',
+            password='testpassword'
+        ), follow_redirects=True)
+
+        # enter income
+        response = self.app.post('/enterincome', data=dict(
+            user='testuser',
+            name='Salary',
+            amount='5000',
+            date='2023-12-06'
+        ), follow_redirects=True)
+
+        # income is displayed on the page
+        self.assertIn(b'Salary', response.data)
+        self.assertIn(b'$5000', response.data)
+        self.assertIn(b'2023-12-06', response.data)
+
+        # sign out
+        response = self.app.get('/signout', follow_redirects=True)
+        self.assertNotIn(b'Logged in as', response.data)
+        self.assertIn(b'Sign In Here!', response.data)
+
+    # test for recording page
+    def test_view_record_expenses(self):
+        # sign up
+        self.app.post('/signup', data=dict(
+            user='testuser',
+            password='testpassword',
+            password2='testpassword'
+        ), follow_redirects=True)
+
+        # log in
+        self.app.post('/signin', data=dict(
+            user='testuser',
+            password='testpassword'
+        ), follow_redirects=True)
+
+        # expense
+        self.app.post('/enterexpense', data=dict(
+            user='testuser',
+            name='Christmas',
+            amount='100',
+            date='2023-12-06'
+        ), follow_redirects=True)
+
+        # record 
+        response = self.app.get('/record', follow_redirects=True)
+
+        # entered expense is on page
+        self.assertIn(b'Christmas', response.data)
+        self.assertIn(b'$100', response.data)
+        self.assertIn(b'2023-12-06', response.data)
+
+        # sign out
+        response = self.app.get('/signout', follow_redirects=True)
+        self.assertNotIn(b'Logged in as', response.data)
+        self.assertIn(b'Sign In Here!', response.data)
+
+
 if __name__ == '__main__':
     unittest.main()
